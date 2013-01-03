f546ba8d57c5c5f9e46353b848a6fc6ecc7ae5b0
Denis.Dydychkin <denis.dydychkin@lanit-tercom.com>
2013-01-03 18:15:58 +0400

    LCDC registers setup

--- include/linux/da8xx-ili9340-fb.h
+++ include/linux/da8xx-ili9340-fb.h
@@ -1,6 +1,27 @@
 #ifndef __DA8XX_ILI9340_PLATFORM_DATA
 #define __DA8XX_ILI9340_PLATFORM_DATA __FILE__
 
+
+enum da8xx_ili9340_pdata_lcdc_lidd_bus_mode {
+	DA8XX_LCDC_LIDD_MODE_INVALID = 0,
+	DA8XX_LCDC_LIDD_MODE_6800SYNC,
+	DA8XX_LCDC_LIDD_MODE_6800ASYNC,
+	DA8XX_LCDC_LIDD_MODE_8080SYNC,
+	DA8XX_LCDC_LIDD_MODE_8080ASYNC,
+};
+
+enum da8xx_ili9340_pdata_lcdc_lidd_bus_cs {
+	DA8XX_LCDC_LIDD_CS0 = 0,
+	DA8XX_LCDC_LIDD_CS1,
+};
+
+enum da8xx_ili9340_pdata_lcdc_lidd_bus_polarity {
+	DA8XX_LCDC_LIDD_POL_DEFAULT = 0,
+	DA8XX_LCDC_LIDD_POL_ACTIVE_HIGH,
+	DA8XX_LCDC_LIDD_POL_ACTIVE_LOW,
+};
+
+
 struct da8xx_ili9340_pdata {
 	int	xres;
 	int	yres;
@@ -10,7 +31,26 @@ struct da8xx_ili9340_pdata {
 	int	screen_width;
 	int	fps;
 
-	int	lidd_cs;
+	enum da8xx_ili9340_pdata_lcdc_lidd_bus_mode	lcdc_lidd_mode;
+	enum da8xx_ili9340_pdata_lcdc_lidd_bus_cs	lcdc_lidd_cs;		//async mode only
+	enum da8xx_ili9340_pdata_lcdc_lidd_bus_polarity	lcdc_lidd_ale_pol;
+	enum da8xx_ili9340_pdata_lcdc_lidd_bus_polarity	lcdc_lidd_rs_en_pol;
+	enum da8xx_ili9340_pdata_lcdc_lidd_bus_polarity	lcdc_lidd_ws_dir_pol;
+	enum da8xx_ili9340_pdata_lcdc_lidd_bus_polarity	lcdc_lidd_cs_pol;
+	enum da8xx_ili9340_pdata_lcdc_lidd_bus_polarity	lcdc_lidd_reset_pol;
+	unsigned long					lcdc_lidd_edma_burst;
+
+	unsigned long					lcdc_lidd_mclk_ns;
+	unsigned long 					lcdc_t_ta_ns;
+	unsigned long					lcdc_t_rhold_ns;
+	unsigned long					lcdc_t_rstrobe_ns;
+	unsigned long					lcdc_t_rsu_ns;
+	unsigned long					lcdc_t_whold_ns;
+	unsigned long					lcdc_t_wstrobe_ns;
+	unsigned long					lcdc_t_wsu_ns;
+	unsigned long					lcdc_t_reset_ns;
+	unsigned long					lcdc_t_reset_wait_ns;
+
 };
 
 
