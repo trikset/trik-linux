b050102c045fb497ccec39007326206fde7785ad
Denis.Dydychkin <denis.dydychkin@lanit-tercom.com>
2013-01-10 22:33:08 +0400

    Removed VISUAL_888 mode - impossible to support it with ARM's endianess Fixed color's offsets for 32-bits mode Added visual_mode_red_blue_swap for newheaven display with messed red and blue outputs Added pdata for landscape mode; made default

--- include/linux/da8xx-ili9340-fb.h
+++ include/linux/da8xx-ili9340-fb.h
@@ -5,8 +5,7 @@
 enum da8xx_ili9340_pdata_lcdc_visual_mode {
 	DA8XX_LCDC_VISUAL_INVALID = 0,
 	DA8XX_LCDC_VISUAL_565,	// 16bps
-	DA8XX_LCDC_VISUAL_888,	// 24bps, 8bits per color, but only 6 topmost are actually used
-	DA8XX_LCDC_VISUAL_8880,	// 32bps, 8bits per color + 1 empty byte
+	DA8XX_LCDC_VISUAL_8880,	// 32bps, 6bits per color, byte-aligned and 1 empty byte
 };
 
 enum da8xx_ili9340_pdata_lcdc_lidd_bus_mode {
@@ -43,6 +42,7 @@ struct da8xx_ili9340_pdata {
 	bool	yflip;
 	bool	xyswap;
 	enum da8xx_ili9340_pdata_lcdc_visual_mode	visual_mode;
+	bool    visual_mode_red_blue_swap;	// fix for a display with messed red and blue components
 
 	int	screen_height;
 	int	screen_width;
