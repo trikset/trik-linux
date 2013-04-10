4941f6773e31ebaa94ce64848b148ad788aac722
Roman Meshkevich <roman.meshkevich@lanit-tercom.com>
2013-04-10 13:38:01 +0400

    MPU 6050 add some fix

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -65,6 +65,7 @@ static const char * const iio_chan_type_name_spec[] = {
 	[IIO_CAPACITANCE] = "capacitance",
 	[IIO_ALTVOLTAGE] = "altvoltage",
 	[IIO_CCT] = "cct",
+	[IIO_QUATERNION] = "quaternion",
 };
 
 static const char * const iio_modifier_names[] = {
@@ -79,6 +80,7 @@ static const char * const iio_modifier_names[] = {
 	[IIO_MOD_LIGHT_RED] = "red",
 	[IIO_MOD_LIGHT_GREEN] = "green",
 	[IIO_MOD_LIGHT_BLUE] = "blue",
+        [IIO_MOD_R]  = "r",
 };
 
 /* relies on pairs of these shared then separate */
