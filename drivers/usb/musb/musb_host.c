89af00bd57bc988412fe8261c3bd218f5da78823
Denis Dydychkin <nyrl@mail.ru>
2013-06-18 21:05:45 +0400

    Linux-Davinci merging: commit 6eec862e2fce798313a184a6cb4e695dec78ddab Author: Ajay Kumar Gupta <ajay.gupta@ti.com> Date: Thu May 24 15:42:53 2012 +0530
        usb: musb: host: Flush txfifo only if TxPktRdy bit set
    
        This is needed as per mentor core documents.

--- drivers/usb/musb/musb_host.c
+++ drivers/usb/musb/musb_host.c
@@ -135,23 +135,26 @@ static void musb_h_tx_flush_fifo(struct musb_hw_ep *ep)
 	struct musb	*musb = ep->musb;
 	void __iomem	*epio = ep->regs;
 	u16		csr;
-	u16		lastcsr = 0;
 	int		retries = 1000;
 
 	csr = musb_readw(epio, MUSB_TXCSR);
-	while (csr & MUSB_TXCSR_FIFONOTEMPTY) {
-		if (csr != lastcsr)
+	while (csr & MUSB_TXCSR_TXPKTRDY) {
+		if (!(csr & MUSB_TXCSR_FIFONOTEMPTY))
+			return;
+		else
 			dev_dbg(musb->controller,
 				"Host TX FIFONOTEMPTY csr: %02x\n", csr);
-		lastcsr = csr;
+
 		csr |= MUSB_TXCSR_FLUSHFIFO;
 		musb_writew(epio, MUSB_TXCSR, csr);
 		csr = musb_readw(epio, MUSB_TXCSR);
-		if (WARN(retries-- < 1,
+
+		if (retries-- < 1) {
+			dev_dbg(musb->controller,
 				"Could not flush host TX%d fifo: csr: %04x\n",
-				ep->epnum, csr))
+					ep->epnum, csr);
 			return;
-		mdelay(1);
+		}
 	}
 }
 
