5fe6cf252cbc2884d09276a2a37e6e53143ea612
Roman Meshkevich <roman.meshkevich@lanit-tercom.com>
2014-11-14 17:40:14 +0300

    Initial jdx platform driver

---
+++ include/linux/trik-jdx.h
@@ -0,0 +1,16 @@
+#ifndef __TRIK_JDX_H__
+#define __TRIK_JDX_H__
+
+
+struct trik_jd1_platform_data {
+	int gpio_d1a;
+	int gpio_d1e;
+	int gpio_d1b;
+};
+
+struct trik_jd2_platform_data {
+	int gpio_d2a;
+	int gpio_d2e;
+	int gpio_d2b;
+};
+#endif  /* __JDX_H__ */
