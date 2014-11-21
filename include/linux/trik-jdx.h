0129773d72667e7e1bfb8e493c8a6a633e524cc8
Roman Meshkevich <roman.meshkevich@lanit-tercom.com>
2014-11-21 18:32:33 +0300

    add comments

--- include/linux/trik-jdx.h
+++ include/linux/trik-jdx.h
@@ -3,14 +3,20 @@
 
 
 struct trik_jd1_platform_data {
+	//GPIO_TO_PIN(3,3)
 	int gpio_d1a;
+	//GPIO_TO_PIN(0,13)
 	int gpio_d1e;
+	//GPIO_TO_PIN(3,2)
 	int gpio_d1b;
 };
 
 struct trik_jd2_platform_data {
+	//GPIO_TO_PIN(3,1)
 	int gpio_d2a;
+	//GPIO_TO_PIN(0,11)
 	int gpio_d2e;
+	//GPIO_TO_PIN(3,4)
 	int gpio_d2b;
 };
 #endif  /* __JDX_H__ */
