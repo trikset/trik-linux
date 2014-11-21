127fa3c73c7438887c24715593ed5e0162673d57
Roman Meshkevich <roman.meshkevich@lanit-tercom.com>
2014-11-21 18:33:30 +0300

    add first platform device registeration

--- drivers/trik/jd1/jd1_hscr04.c
+++ drivers/trik/jd1/jd1_hscr04.c
@@ -1,16 +1,56 @@
 #include <linux/module.h>  
 #include <linux/kernel.h>  
+#include <linux/platform_device.h>
+#include <linux/gpio.h>
+#include <mach/mux.h>
 
+#include <linux/trik-jdx.h>
+
+
+
+static struct platform_device da850_trik_jd1_device = {
+	.name			= "da850_trik_jd1",
+	.id				= -1,
+	.num_resources	= 0,
+};
+
+static struct trik_jd1_platform_data da850_trik_jd1_pdata = {
+	.gpio_d1a = GPIO_TO_PIN(3,3),
+	.gpio_d1e = GPIO_TO_PIN(0,13),
+	.gpio_d1b = GPIO_TO_PIN(3,2),
+};
+static const short da850_trik_jd1_pins[] = {
+	DA850_GPIO3_3,
+	DA850_GPIO3_2,
+	DA850_GPIO0_13,
+	-1
+};
 int init_module(void)
 {
-	return 0;
+	int ret;
+	ret = davinci_cfg_reg_list(da850_trik_jd1_pins);
+	if(ret){
+		pr_err("%s: trik jd1 pins failed: %d\n",__func__,ret);
+		goto exit_mux_failed;
+	}
+
+	ret = platform_device_register(&da850_trik_jd1_device);
+	if (ret){
+		pr_err("%s: trik jd1 device register failed: %d\n",__func__,ret);
+		goto exit_register_failed;
+	}
+exit_register_failed:
+exit_mux_failed:
+	return ret;
 }
 
 
 void cleanup_module(void)
 {
-
+	platform_device_unregister(&da850_trik_jd1_device);
 }
 
+module_init(init_module);
+module_exit(cleanup_module);
 MODULE_LICENSE("GPL");
 
